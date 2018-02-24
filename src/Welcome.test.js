04b483592ae63af2745d36212417753cc8e4cd11
Andres Urrea <andres.urreao@gmail.com>
2018-02-23 22:43:10 -0500

    Second-import

--- src/Welcome.test.js
+++ src/Welcome.test.js
@@ -4,7 +4,7 @@ import Welcome from './Welcome';
 import Setup from './Setup';
 
 it('renders Welcome without crashing', () => {
-  shallow(<Welcome />)
+  shallow(<Welcome />);
 });
 
 it('renders an h1', () => {
